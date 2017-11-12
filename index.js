2425ed8ad1ca12e80ce7b8aa014db07c03f9c992
makstheimba <makstheimba@yandex.ru>
2017-11-12 15:37:10 +0300

    Add icons, titles and balance forces

--- index.js
+++ index.js
@@ -3,24 +3,23 @@ require('d3-selection-multi');
 
 window.onload = () => {
   const url = 'https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json';
-  const [width, height] = [1000, 400];
+  const [width, height] = [1280, 720];
   const svg = d3.select('.chart').attr('viewBox', `0 0 ${width} ${height}`);
 
   d3.json(url, (error, countrys) => {
     const simulation = d3.forceSimulation();
-    const link = svg.append('g')
-      .attr('class', 'links')
-      .selectAll('line')
+    const link = svg.selectAll('line')
       .data(countrys.links)
       .enter()
       .append('line');
     const node = svg.append('g')
-      .attr('class', 'nodes')
       .selectAll('circle')
       .data(countrys.nodes)
       .enter()
+      .append('g')
+      .attr('transform', 'translate(-8, -6)')
       .append('image')
-      .attrs(({ code }) => ({ width: 12, height: 9, 'xlink:href': `flags/4x3/${code}.svg` }))
+      .attrs(({ code }) => ({ width: 16, height: 12, 'xlink:href': `flags/4x3/${code}.svg` }))
       .call(d3.drag()
         .on('start', (dragNode) => {
           if (!d3.event.active) simulation.alphaTarget(0.3).restart();
@@ -37,11 +36,14 @@ window.onload = () => {
           dragNode.fy = null;
         }));
 
+    node.append('title').text(({ country }) => country);
     simulation
       .nodes(countrys.nodes)
       .force('link', d3.forceLink(countrys.links))
-      .force('charge', d3.forceManyBody().strength(-2).distanceMax(300))
+      .force('charge', d3.forceManyBody().distanceMax(225))
       .force('center', d3.forceCenter(width / 2, height / 2))
+      .force('collide', d3.forceCollide().radius(9))
+      .force('alignY', d3.forceY(height / 2).strength(0.01))
       .on('tick', () => {
         link.attrs(({ source: { x: x1, y: y1 }, target: { x: x2, y: y2 } }) => ({ x1, x2, y1, y2 }));
         node.attrs(({ x, y }) => ({ x, y }));
